7af9e9bb10fcb8964a426aa53d1b1e6840969b7a
YouanZhang <48627399+YouanZhang@users.noreply.github.com>
2019-12-05 21:56:34 +0800

    init

---
+++ Data Structions/Lab4/Lab4.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+typedef double DB;
+typedef pair <int, int> P;
+typedef pair <int, P> PP;
+const int MAXN = 1e5+7;
+const DB EPS = 1e-9;
+const DB DB_INF = 1e100;
+const DB PI=acos(-1.0);
+const int INT_INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.precision(6); cout << fixed;
+    
+    return 0;
+}
